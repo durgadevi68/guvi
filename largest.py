83e6b7c96415f97dd2e3ee5c18c1703efb6f8bf4
durgadevi68 <50898231+durgadevi68@users.noreply.github.com>
2019-05-23 21:16:33 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,10 +1,14 @@
-a=int(input())
-b=int(input())
-c=int(input())
+d=input()
+x=d.split()
+a=int(x[0])
+b=int(x[1])
+c=int(x[2])
 if(a>b and a>c):
-  print(a)
+    print(a)
 elif(b>a and b>c):
-  print(b)
+    print(b)
+elif(c>a and c>b):
+    print(c)
 else:
-  print(c)
+    print("invalid")
 
