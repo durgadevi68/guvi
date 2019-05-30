1073c62506828c862d0a0bd3cd2111395ffe048e
durgadevi68 <50898231+durgadevi68@users.noreply.github.com>
2019-05-30 21:52:45 +0530

    Create onecharacter.py

---
+++ onecharacter.py
@@ -0,0 +1,12 @@
+str1,str2=map(str,input().split())
+count=0
+for i in range(0,len(str1)):
+    if(str1[i]!=str2[i]):
+       count=count+1
+    else:
+       continue
+if count==1:
+    print("yes")
+else:
+    print("no")
+   
