bffb3f4ba086438e720f48447b3b1482899c5f24
durgadevi68 <50898231+durgadevi68@users.noreply.github.com>
2019-06-01 15:34:29 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,8 @@
+w=input(" ")
+if(w=='monday' or w=='tueday' or w=='wednesday' or w=='thruday' or w=='friday'):
+    print("no")
+elif(w=='saturday' or w=='sunday'):
+    print("yes")
+else:
+    print("invalid")
+
